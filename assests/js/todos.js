0f91309ee31064a9afc47f079626e937256302fb
Rahul A Thawal <rthawal2014@my.fit.edu>
2018-08-04 11:21:46 -0400

    Fadeout List, Line through

--- assests/js/todos.js
+++ assests/js/todos.js
@@ -1,1 +1,24 @@
-alert("CONNECTED");
+$("li").click(function(){
+    //Use of ToggleClass for efficient Code.
+    console.log($(this).css("color"));
+    if($(this).css("color") === "rgb(255, 0, 0)"){
+        $(this).css({
+            color:"black",
+            textDecoration:"none"
+        });
+    }
+    else{
+        $(this).css({
+            color: "red",
+            textDecoration: "line-through"
+        });
+    }
+    
+});
+
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
